f7696228ef3db6d1e5391fb5bac554e3d131cd58
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-01-20 12:24:25 -0300

    fix: changing classes to hooks

--- src/components/Modals/TransactionDetailModal/TransactionDetailModal.tsx
+++ src/components/Modals/TransactionDetailModal/TransactionDetailModal.tsx
@@ -1,39 +1,45 @@
 import * as React from 'react'
-import { Close } from 'decentraland-ui'
-import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
+import { Close, ModalProps } from 'decentraland-ui'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import './TransactionDetailModal.css'
 
-export default class TransactionDetailModal extends React.PureComponent<ModalProps> {
-  render() {
-    const { name, onClose } = this.props
-
-    return (
-      <Modal
-        name={name}
-        className="TransactionDetailModal"
-        closeIcon={<Close onClick={onClose} />}
-      >
-        <Modal.Header>Transaction Detail</Modal.Header>
-        <Modal.Content>
-          <div className="data">
-            <div> Operation </div>
-            <div> Withdraw to Matic Network </div>
-          </div>
-          <div className="data">
-            <div> Amount </div>
-            <div> 15,000 USD </div>
-          </div>
-          <div className="data">
-            <div> Initialized </div>
-            <div> Dec 13 - 14pm </div>
-          </div>
-          <div className="data">
-            <div> Status </div>
-            <div> Completed </div>
-          </div>
-        </Modal.Content>
-      </Modal>
-    )
-  }
+type TransactionProps = {
+  operation: string
+  amount: number
+  initialilzed: string
+  status: string
 }
+
+type Props = ModalProps | TransactionProps
+
+const TransactionDetailModal = ({ name, onClose }: Props) => {
+  return (
+    <Modal
+      name={name}
+      className="TransactionDetailModal"
+      closeIcon={<Close onClick={onClose} />}
+    >
+      <Modal.Header>Transaction Detail</Modal.Header>
+      <Modal.Content>
+        <div className="data">
+          <div> Operation </div>
+          <div> Withdraw to Matic Network </div>
+        </div>
+        <div className="data">
+          <div> Amount </div>
+          <div> 15,000 USD </div>
+        </div>
+        <div className="data">
+          <div> Initialized </div>
+          <div> Dec 13 - 14pm </div>
+        </div>
+        <div className="data">
+          <div> Status </div>
+          <div> Completed </div>
+        </div>
+      </Modal.Content>
+    </Modal>
+  )
+}
+
+export default React.memo(TransactionDetailModal)
