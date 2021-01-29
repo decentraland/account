48353795157660e7d2568179425a05c569290e11
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-01-29 15:47:48 -0300

    Revert "fix: with wrong copied file on a wrong folder"
    This reverts commit a0b90eecc505804b7ec480188698054fb9177501.

--- src/components/AccountHeader/AccountHeader.container.ts
+++ src/components/AccountHeader/AccountHeader.container.ts
@@ -1,26 +1,26 @@
-import { RootState } from '../../../modules/reducer'
+import { RootState } from '../../modules/reducer'
 import { getData as getProfiles } from 'decentraland-dapps/dist/modules/profile/selectors'
 import {
   MapStateProps,
   MapDispatchProps,
-  MapDispatch
-} from './AccountHeader.types'
+  MapDispatch,
+} from '../HomePage/AccountHeader/AccountHeader.types'
 import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 import { connect } from 'react-redux'
-import AccountHeader from './AccountHeader'
+import { AccountHeader } from '../HomePage/AccountHeader'
 
 const mapState = (state: RootState): MapStateProps => {
   const address = getAddress(state)
   const profiles = getProfiles(state)
   const profile = address && address in profiles ? profiles[address] : null
   return {
-    avatar: profile && profile.avatars.length > 0 ? profile.avatars[0] : null
+    avatar: profile && profile.avatars.length > 0 ? profile.avatars[0] : null,
   }
 }
 
 const mapDispatch = (_dispatch: MapDispatch): MapDispatchProps => ({
-  onChangeAlias: () => {},
-  onEditAvatar: () => {}
+  onChangeAlias: () => { },
+  onEditAvatar: () => { },
 })
 
 export default connect(mapState, mapDispatch)(AccountHeader)
