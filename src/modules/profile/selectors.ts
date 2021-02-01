2a6418a6d50ae0fc69db512b9aba46ee8887bddd
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-01 11:45:29 -0300

    fix: fixes based in feedback

---
+++ src/modules/profile/selectors.ts
@@ -0,0 +1,12 @@
+import { getData as getProfiles } from 'decentraland-dapps/dist/modules/profile/selectors'
+import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
+import { createSelector } from 'reselect'
+
+export const getProfile = createSelector(
+  getAddress,
+  getProfiles,
+  (address, profiles) => {
+    const profile = address && address in profiles ? profiles[address] : null
+    return profile && profile.avatars.length > 0 ? profile.avatars[0] : null
+  }
+)
