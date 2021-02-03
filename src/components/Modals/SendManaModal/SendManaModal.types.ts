397e30d33479898fa2bfccdbedffba062b52454b
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-03 12:01:44 -0300

    feat: send mana

--- src/components/Modals/SendManaModal/SendManaModal.types.ts
+++ src/components/Modals/SendManaModal/SendManaModal.types.ts
@@ -2,10 +2,11 @@ import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import {
   fetchManaPriceRequest,
-  sendManaRequest,
+  sendManaRequest
 } from '../../../modules/mana/actions'
 
 export type Props = ModalProps & {
+  isLoading: boolean
   manaPrice: number
   onSendMana: typeof sendManaRequest
   onManaPrice: typeof fetchManaPriceRequest
