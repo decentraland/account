4ca0b24993f63f249041ad20fe8d9c2e5e517d67
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-03 14:46:48 -0300

    feat: using menu for send and receive

--- src/components/HomePage/AccountCard/AccountCardHeader/AccountCardHeader.tsx
+++ src/components/HomePage/AccountCard/AccountCardHeader/AccountCardHeader.tsx
@@ -1,30 +1,47 @@
 import React from 'react'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
-import { Button, Icon } from 'decentraland-ui'
+import { Button, Dropdown, Icon } from 'decentraland-ui'
 import { NetworkType } from '../../HomePage.types'
 import { Props } from './AccountCardHeader.types'
 import './AccountCardHeader.css'
 
-const AccountCardHeader = ({ type, title, amount, onSendMana }: Props) => (
-  <div className="AccountCardHeader">
-    <div className="title">{title}</div>
-    <div className="fundsContainer">
-      <div className="funds">
-        <div className="amount">
-          <div
-            className={type === NetworkType.MATIC ? 'matic-logo' : 'mana-logo'}
-          />
-          {amount}
+const AccountCardHeader = ({ type, title, amount, onSendMana }: Props) => {
+  return (
+    <div className="AccountCardHeader">
+      <div className="title">
+        {title}
+        <div className="operation-menu">
+          <Dropdown text="..." direction="left">
+            <Dropdown.Menu>
+              <Dropdown.Item text="Send" onClick={() => onSendMana()} />
+              <Dropdown.Item
+                text="Receive"
+                onClick={() => alert('must be implemented')}
+              />
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
       </div>
-      <div className="actions">
-        <Button onClick={() => onSendMana()}>
-          <Icon name="plus" />
-        </Button>
-        <Button>{t('account_card_header.convert')}</Button>
+      <div className="fundsContainer">
+        <div className="funds">
+          <div className="amount">
+            <div
+              className={
+                type === NetworkType.MATIC ? 'matic-logo' : 'mana-logo'
+              }
+            />
+            {amount}
+          </div>
+        </div>
+        <div className="actions">
+          <Button>
+            <Icon name="plus" />
+          </Button>
+          <Button>{t('account_card_header.convert')}</Button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default React.memo(AccountCardHeader)
