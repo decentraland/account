5ee3dadb2638311b9d90a6c887879568383fbabc
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-03 12:24:36 -0300

    wip: fixing changes

--- src/components/HomePage/AccountHeader/AccountHeader.container.ts
+++ src/components/HomePage/AccountHeader/AccountHeader.container.ts
@@ -1,22 +1,16 @@
 import { RootState } from '../../../modules/reducer'
-import { getData as getProfiles } from 'decentraland-dapps/dist/modules/profile/selectors'
 import {
   MapStateProps,
   MapDispatchProps,
   MapDispatch,
 } from './AccountHeader.types'
-import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 import { connect } from 'react-redux'
+import { getProfile } from '../../../modules/profile/selectors'
 import AccountHeader from './AccountHeader'
 
-const mapState = (state: RootState): MapStateProps => {
-  const address = getAddress(state)
-  const profiles = getProfiles(state)
-  const profile = address && address in profiles ? profiles[address] : null
-  return {
-    avatar: profile && profile.avatars.length > 0 ? profile.avatars[0] : null,
-  }
-}
+const mapState = (state: RootState): MapStateProps => ({
+  avatar: getProfile(state),
+})
 
 const mapDispatch = (_dispatch: MapDispatch): MapDispatchProps => ({
   onChangeAlias: () => {},
