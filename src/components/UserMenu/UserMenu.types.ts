e34c11056b5111970782c201f7cad837bde16372
Juan Cazala <juancazala@gmail.com>
2021-02-10 14:32:09 -0300

    feat: cosas

--- src/components/UserMenu/UserMenu.types.ts
+++ src/components/UserMenu/UserMenu.types.ts
@@ -1,48 +1,13 @@
 import { Dispatch } from 'redux'
 import { CallHistoryMethodAction } from 'connected-react-router'
-import {
-  ConnectWalletRequestAction,
-  DisconnectWalletAction,
-} from 'decentraland-dapps/dist/modules/wallet/actions'
-import { Profile } from 'decentraland-dapps/dist/modules/profile/types'
+import { UserMenuProps } from 'decentraland-ui'
 
-export type Props = {
-  isLoggedIn: boolean
-  isLoggingIn: boolean
-  address?: string
-  mana?: number
-  profile?: Profile
-  onLogout: () => void
-  onLogin: () => void
-  hasPendingTransactions: boolean
-  isActivityPage: boolean
-  onNavigateToActivityPage: () => void
-  onNavigateToSettingsPage: () => void
-  menuItems?: React.ReactNode[]
-}
+export type Props = UserMenuProps
 
-export type State = {
-  isOpen: boolean
-}
-
-export type MapStateProps = Pick<
-  Props,
-  | 'isLoggedIn'
-  | 'isLoggingIn'
-  | 'address'
-  | 'profile'
-  | 'mana'
-  | 'hasPendingTransactions'
-  | 'isActivityPage'
->
+export type MapStateProps = Pick<Props, 'isActivity'>
 export type MapDispatchProps = Pick<
   Props,
-  | 'onLogin'
-  | 'onLogout'
-  | 'onNavigateToActivityPage'
-  | 'onNavigateToSettingsPage'
+  'onClickSettings' | 'onClickActivity'
 >
-export type MapDispatch = Dispatch<
-  ConnectWalletRequestAction | DisconnectWalletAction | CallHistoryMethodAction
->
+export type MapDispatch = Dispatch<CallHistoryMethodAction>
 export type OwnProps = Partial<Props>
