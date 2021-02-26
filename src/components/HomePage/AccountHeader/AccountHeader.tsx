728b37c3abc964134e9f3ac1b34d3856aa8341f3
Marcelo Alaniz <alanizmarcelo@gmail.com>
2021-02-26 12:03:03 -0300

    feat: links for header buttons. Change Alias & Edit Avatar

--- src/components/HomePage/AccountHeader/AccountHeader.tsx
+++ src/components/HomePage/AccountHeader/AccountHeader.tsx
@@ -4,8 +4,20 @@ import { AvatarFace, Button, Header } from 'decentraland-ui'
 import { Props } from './AccountHeader.types'
 import './AccountHeader.css'
 
+const BUILDER_URL = process.env.REACT_APP_BUILDER_URL!
+const EXPLORER_URL = process.env.REACT_APP_EXPLORER_URL!
+
 const AccountHeader = (props: Props) => {
   const { avatar } = props
+
+  const handleChangeAlias = () => {
+    window.open(`${BUILDER_URL}/names`, '_blank')
+  }
+
+  const handleEditAvatar = () => {
+    window.open(`${EXPLORER_URL}/?OPEN_AVATAR_EDITOR`, '_blank')
+  }
+
   return (
     <Header size="large" className="AccountHeader">
       <AvatarFace size="large" avatar={avatar || undefined} />
@@ -18,10 +30,10 @@ const AccountHeader = (props: Props) => {
         </div>
       </div>
       <div className="actions">
-        <Button primary inverted>
+        <Button primary inverted onClick={handleChangeAlias}>
           {t('account_header.actions.change_alias')}
         </Button>
-        <Button primary inverted>
+        <Button primary inverted onClick={handleEditAvatar}>
           {t('account_header.actions.edit_avatar')}
         </Button>
       </div>
