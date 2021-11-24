7544535cd13d1669a086a616b36f4d5c4c3ca36a
Lautaro Petaccio <lausuper@gmail.com>
2021-11-24 15:28:31 -0300

    fix: Tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,4 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-jest.mock('decentraland-dapps/dist/modules/analytics/snippet')
+document.body.append(document.createElement('script'))
