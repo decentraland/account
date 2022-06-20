04e0cff59e3359f85f3d42265d083915e4df0b44
Juan Cazala <juancazala@gmail.com>
2022-06-20 17:07:44 -0300

    ci: destroy old infra

--- .ci/index.ts
+++ .ci/index.ts
@@ -1,17 +1,4 @@
-import { env, envTLD } from 'dcl-ops-lib/domain'
-import { buildStatic } from 'dcl-ops-lib/buildStatic'
-
 async function main() {
-  const account = buildStatic({
-    domain: `account.decentraland.${env === 'prd' ? 'org' : envTLD}`,
-    defaultPath: 'index.html',
-    unprotect: true,
-    destroy: true,
-  })
-
-  return {
-    cloudfrontDistribution: account.cloudfrontDistribution,
-    bucketName: account.contentBucket,
-  }
+  return {}
 }
 export = main
