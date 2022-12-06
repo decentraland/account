2e1f2b3b1961889985b0a8b26d2dee7d7c18d1b8
Kevin Szuchet <kevin.szuchet@decentraland.org>
2022-12-06 11:04:12 -0300

    style: Remove comment

--- src/components/HomePage/AccountCard/AccountCardHeader/AccountCardHeader.tsx
+++ src/components/HomePage/AccountCard/AccountCardHeader/AccountCardHeader.tsx
@@ -33,7 +33,6 @@ const AccountCardHeader = ({
 
   const handleAddTokens = () => {
     setIsOpenBuyManaWithFiatModal(true)
-    // onPurchase(network)
   }
 
   const handleCloseBuyManaWithFiatModal = () => {
