cfd0da7b313b14d2e290b45d6d673dfe045b28c3
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-08-04 16:30:00 -0300

    fix: Upgrade decentraland ui and dapps packages

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,21 @@
 import React, { useCallback } from 'react'
-import { Navbar as BaseNavbar, UserMenu } from 'decentraland-dapps/dist/containers'
+import {
+  Navbar as BaseNavbar,
+  UserMenu,
+} from 'decentraland-dapps/dist/containers'
 
 import { locations } from '../../modules/locations'
 import { Props } from './Navbar.types'
 import './Navbar.css'
 
 const Navbar = (props: Props) => {
-  const { pathname, onNavigate, isConnected } = props
+  const { pathname, onNavigate, isConnected, isProfileSiteEnabled } = props
 
   if (isConnected) {
-    props = { ...props, rightMenu: <UserMenu /> }
+    props = {
+      ...props,
+      rightMenu: <UserMenu newMenu={isProfileSiteEnabled} />,
+    }
   }
 
   const handleOnSignIn = useCallback(() => {
