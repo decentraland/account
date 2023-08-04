01644e4b619f2ff46f1fd74b99257fdfd9c30486
Gabriel Díaz <gabrieldiaz31@gmail.com>
2023-08-04 17:22:54 -0300

    fix: Upgrade decentraland ui and dapps packages (#294)

--- src/components/Navbar/Navbar.types.ts
+++ src/components/Navbar/Navbar.types.ts
@@ -5,9 +5,13 @@ import { NavbarProps } from 'decentraland-ui'
 export type Props = Partial<NavbarProps> & {
   pathname: string
   isConnected: boolean
+  isProfileSiteEnabled: boolean
   onNavigate: (path: string) => void
 }
 
-export type MapStateProps = Pick<Props, 'pathname' | 'isConnected'>
+export type MapStateProps = Pick<
+  Props,
+  'pathname' | 'isConnected' | 'isProfileSiteEnabled'
+>
 export type MapDispatchProps = Pick<Props, 'onNavigate'>
 export type MapDispatch = Dispatch<CallHistoryMethodAction>
