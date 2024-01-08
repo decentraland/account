5af4f32863d7129e1a87a3db82356622c2b2000d
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-01-08 23:04:01 +0100

    feat: Add NavbarV2

--- src/components/Navbar/Navbar.types.ts
+++ src/components/Navbar/Navbar.types.ts
@@ -3,12 +3,21 @@ import { CallHistoryMethodAction } from 'connected-react-router'
 import { NavbarProps } from 'decentraland-ui'
 
 export type Props = Partial<NavbarProps> & {
+  hasActivity: boolean
   pathname: string
   isConnected: boolean
   isAuthDappEnabled: boolean
+  isNavbarV2Enabled: boolean
   onNavigate: (path: string) => void
 }
 
-export type MapStateProps = Pick<Props, 'pathname' | 'isConnected' | 'isAuthDappEnabled'>
+export type MapStateProps = Pick<
+  Props,
+  | 'pathname'
+  | 'hasActivity'
+  | 'isConnected'
+  | 'isAuthDappEnabled'
+  | 'isNavbarV2Enabled'
+>
 export type MapDispatchProps = Pick<Props, 'onNavigate'>
 export type MapDispatch = Dispatch<CallHistoryMethodAction>
