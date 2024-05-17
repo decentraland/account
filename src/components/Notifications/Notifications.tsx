f712c43ea16769dc7bcd139a2aeca46279e489c9
Braian Mellor <braianj@gmail.com>
2024-05-17 11:34:44 -0300

    fix: remove unused react import

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { SubscriptionGroupKeys } from '../../modules/subscription/types'
