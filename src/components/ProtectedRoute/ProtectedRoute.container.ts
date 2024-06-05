79ff85cb35e9f259cb620c9c42730c7943d3ea93
Braian Mellor <braianj@gmail.com>
2024-06-05 16:23:10 -0300

    fix: reconnect when switching wallet (#376)

--- src/components/ProtectedRoute/ProtectedRoute.container.ts
+++ src/components/ProtectedRoute/ProtectedRoute.container.ts
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux'
+import { localStorageGetIdentity } from '@dcl/single-sign-on-client'
 import { getData as getWallet, isConnected, isConnecting } from 'decentraland-dapps/dist/modules/wallet/selectors'
 import { RootState } from '../../modules/reducer'
 import ProtectedRoute from './ProtectedRoute'
 import { MapStateProps } from './ProtectedRoute.types'
 
 const mapState = (state: RootState): MapStateProps => {
+  const wallet = getWallet(state)
+  const identity = wallet ? localStorageGetIdentity(wallet?.address) : null
+
   return {
-    wallet: getWallet(state),
     isConnecting: isConnecting(state),
-    isConnected: isConnected(state)
+    isConnected: isConnected(state) && !!identity
   }
 }
 
--- src/components/ProtectedRoute/ProtectedRoute.types.ts
+++ src/components/ProtectedRoute/ProtectedRoute.types.ts
@@ -1,10 +1,8 @@
 import { RouteProps } from 'react-router-dom'
-import { Wallet } from 'decentraland-dapps/dist/modules/wallet/types'
 
 export type Props = RouteProps & {
   isConnected: boolean
   isConnecting: boolean
-  wallet: Wallet | null
 }
 
-export type MapStateProps = Pick<Props, 'wallet' | 'isConnecting' | 'isConnected'>
+export type MapStateProps = Pick<Props, 'isConnecting' | 'isConnected'>
