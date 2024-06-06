dff426a14f43924e59efcdaa3ad650bd37a6fb35
Braian Mellor <braianj@gmail.com>
2024-06-06 19:10:30 -0300

    feat: add address and copy button to wallets

---
+++ src/components/Wallets/Wallets.styled.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+import { Typography as TypographyMui } from 'decentraland-ui2'
+import { Description as DescriptionGlobal } from '../Typography'
+
+export const Header = styled('div')({ marginLeft: '24px', marginTop: '14px' })
+
+export const Description = styled(DescriptionGlobal)({ display: 'flex', alignItems: 'center' })
+
+export const Address = styled(TypographyMui)({
+  color: '#fcfcfc99',
+  fontSize: '14px',
+  lineHeight: '24px',
+  fontWeight: 500,
+  marginLeft: '8px',
+  marginRight: '8px'
+})
