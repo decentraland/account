00657f7308d8a90c1c73c583127adff73d4ffe41
Braian Mellor <braianj@gmail.com>
2024-06-07 09:10:57 -0300

    feat: move header to left margin

--- src/components/Wallets/Wallets.styled.tsx
+++ src/components/Wallets/Wallets.styled.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { Typography as TypographyMui } from 'decentraland-ui2'
 import { Description as DescriptionGlobal } from '../Typography'
 
-export const Header = styled('div')({ marginLeft: '24px', marginTop: '14px' })
+export const Header = styled('div')({ marginTop: '14px' })
 
 export const Description = styled(DescriptionGlobal)({ display: 'flex', alignItems: 'center' })
 
