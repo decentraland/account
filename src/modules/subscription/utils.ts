577aded69c6441df3da6d961c1a813afd9eddd85
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-04-23 12:29:53 -0300

    fix: sorting

--- src/modules/subscription/utils.ts
+++ src/modules/subscription/utils.ts
@@ -10,6 +10,12 @@ export const subscriptionGroups: SubscriptionGroups = {
     NotificationType.LAND_RENTAL_ENDED,
     NotificationType.LAND_RENTED
   ],
+  [SubscriptionGroupKeys.CREDITS]: [
+    NotificationType.CREDITS_REMINDER_COMPLETE_GOALS,
+    NotificationType.CREDITS_REMINDER_CLAIM_CREDITS,
+    NotificationType.CREDITS_REMINDER_USAGE,
+    NotificationType.CREDITS_REMINDER_DO_NOT_MISS_OUT
+  ],
   [SubscriptionGroupKeys.EVENTS]: [NotificationType.EVENTS_STARTED, NotificationType.EVENTS_STARTS_SOON],
   [SubscriptionGroupKeys.REWARDS]: [
     NotificationType.REWARD_ASSIGNED,
@@ -31,12 +37,6 @@ export const subscriptionGroups: SubscriptionGroups = {
     NotificationType.WORLDS_MISSING_RESOURCES,
     NotificationType.WORLDS_PERMISSION_GRANTED,
     NotificationType.WORLDS_PERMISSION_REVOKED
-  ],
-  [SubscriptionGroupKeys.CREDITS]: [
-    NotificationType.CREDITS_REMINDER_COMPLETE_GOALS,
-    NotificationType.CREDITS_REMINDER_CLAIM_CREDITS,
-    NotificationType.CREDITS_REMINDER_USAGE,
-    NotificationType.CREDITS_REMINDER_DO_NOT_MISS_OUT
   ]
 }
 
