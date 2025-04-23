9fbffc23d02c3df7d7704009f576cbbc60149781
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-04-23 14:09:11 -0300

    chore

--- src/components/Notifications/NotificationGroupCard/NotificationGroupCard.tsx
+++ src/components/Notifications/NotificationGroupCard/NotificationGroupCard.tsx
@@ -79,6 +79,8 @@ function NotificationGroupCard(props: Props) {
     )
   }
 
+  console.log('filteredSubscriptionDetails', filteredSubscriptionDetails)
+
   const handleOnChangeNotificationSetting = useCallback(
     (checked: boolean, type: NotificationType) => {
       if (!isLoading) {
