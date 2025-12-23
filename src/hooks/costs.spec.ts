c24750ddbab6683857dcc1a6f6646f63abaab4a4
Braian Mellor <braianj@gmail.com>
2025-12-23 15:12:33 -0300

    feat: add React 18 compatibility fixes
    - Migrate to createRoot API in src/index.tsx
    - Update renderHook import from @testing-library/react in costs.spec.ts
    - Fix middleware type in store.ts for react-redux 8
    - Fix TextEncoder/TextDecoder types in setupTests.ts
    - Dispatch STORAGE_LOAD synchronously in test store to avoid race conditions
    - Use I18nProvider instead of TranslationProvider in test utils to prevent translation fetch clearing preloaded state

--- src/hooks/costs.spec.ts
+++ src/hooks/costs.spec.ts
@@ -1,5 +1,4 @@
-import { waitFor } from '@testing-library/react'
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook, waitFor } from '@testing-library/react'
 import { getEstimatedExitTransactionCost } from '../modules/mana/utils'
 import { useWithdrawalCost } from './costs'
 
